6bb740e4f762f5115c3ba1c144659dfe02e817c0
hanankem <hanank@jfrog.com>
2021-11-10 15:46:00 +0200

    Added SwiftyLib.podspec

---
+++ demoapp2/demoapp2.swift
@@ -0,0 +1,21 @@
+//
+//  demoapp2.swift
+//  demoapp2
+//
+
+//
+//  SwiftyLib.swift
+//
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
