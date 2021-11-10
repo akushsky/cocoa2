3ae37494f6c135a2d3a54203031ec2bd016222ef
hanankem <hanank@jfrog.com>
2021-11-10 15:41:02 +0200

    Initial project setup

---
+++ demoapp2/demoapp2.h
@@ -0,0 +1,18 @@
+//
+//  demoapp2.h
+//  demoapp2
+//
+//  Created by Hanan Kemelman on 10/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for demoapp2.
+FOUNDATION_EXPORT double demoapp2VersionNumber;
+
+//! Project version string for demoapp2.
+FOUNDATION_EXPORT const unsigned char demoapp2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demoapp2/PublicHeader.h>
+
+
